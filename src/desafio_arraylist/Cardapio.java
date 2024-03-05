1506ff7a4b14d5487c52420a074d79084af095e1
silva3265 <silva3265@gmail.com>
2024-03-05 15:05:30 -0300

    Desafio: ArrayList

---
+++ src/desafio_arraylist/Cardapio.java
@@ -0,0 +1,25 @@
+package desafio_arraylist;
+
+import java.util.ArrayList;
+
+public class Cardapio {
+	
+	ArrayList<ItemCardapio> itens = new ArrayList<>();
+
+    void adicionarItem(ItemCardapio item) {
+        itens.add(item);
+    }
+
+    void removerItem(int indice) {
+        itens.remove(indice);
+    }
+
+    void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
+        for (ItemCardapio item : itens) {
+            if (item.possuiPrecoEntre(precoMinimo, precoMaximo)) {
+                item.imprimir();
+            }
+        }
+    }
+
+}
