67c87545ca056de15b07ea27dd31a37c83199748
silva3265 <silva3265@gmail.com>
2024-03-05 00:43:29 -0300

    Copiando e expandindo arrays

--- src/arrays/Turma.java
+++ src/arrays/Turma.java
@@ -1,14 +1,21 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Turma {
 
 	String identificacao;
-	String nomeProfessora;
-	Aluno[] alunos;
-	
-	void imprimirListaDeAlunos() {
+    String nomeProfessora;
+    Aluno[] alunos = new Aluno[0];
+
+    void adicionarAluno(Aluno aluno) { // Faz uma copia e descarta a anterior
+        alunos = Arrays.copyOf(alunos, alunos.length + 1); // Vai fazer uma copia e adicionando mais uma posição
+        alunos[alunos.length - 1] = aluno; // se ter um array de uma posição, a ultima posição é zero, é sempre length - 1
+    }
+
+    void imprimirListaDeAlunos() {
         for (Aluno aluno : alunos) {
-            if (aluno != null) { // se o aluno nao for nulo
+            if (aluno != null) {
                 System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
             } else {
                 System.out.println("vago");
