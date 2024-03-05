ebd830a92cd1e6014bd8448057c543b1b905467f
silva3265 <silva3265@gmail.com>
2024-03-04 23:57:45 -0300

    Iterando em arrays de objetos

--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
         Turma turmaB = new Turma();
         turmaB.identificacao = "Maternal B";
         turmaB.nomeProfessora = "Tia Maria";
-		turmaB.alunos = new Aluno[3]; // como é um array do tipo 'Aluno', temos que criar uma instancia
+        turmaB.alunos = new Aluno[3];
 
         turmaB.alunos[0] = new Aluno();
         turmaB.alunos[0].nome = "João";
@@ -17,6 +17,18 @@ public class Principal {
         aluno1.idade = 4;
 
         turmaB.alunos[1] = aluno1;
+
+        turmaB.imprimirListaDeAlunos();
+
+//        for (int i = 0; i < turmaB.alunos.length; i++) {
+//            Aluno aluno = turmaB.alunos[i];
+//
+//            if (aluno != null) {
+//                System.out.printf("%d - %s (%d anos)%n", i, aluno.nome, aluno.idade);
+//            } else {
+//                System.out.printf("%d - vago%n", i);
+//            }
+//        }
     }
 
 }
--- src/arrays/Turma.java
+++ src/arrays/Turma.java
@@ -5,5 +5,15 @@ public class Turma {
 	String identificacao;
 	String nomeProfessora;
 	Aluno[] alunos;
+	
+	void imprimirListaDeAlunos() {
+        for (Aluno aluno : alunos) {
+            if (aluno != null) { // se o aluno nao for nulo
+                System.out.printf("%s (%d anos)%n", aluno.nome, aluno.idade);
+            } else {
+                System.out.println("vago");
+            }
+        }
+    }
 
 }
