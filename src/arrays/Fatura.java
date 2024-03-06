75fe2b2e2acbec37324f8e1cb1c9308828d7a777
silva3265 <silva3265@gmail.com>
2024-03-06 15:11:42 -0300

    Criando métodos com argumentos variáveis com Varargs

---
+++ src/arrays/Fatura.java
@@ -0,0 +1,8 @@
+package arrays;
+
+public class Fatura {
+
+	int numero;
+	double valorTotal;
+
+}
--- src/arrays/Principal.java
+++ src/arrays/Principal.java
@@ -1,19 +1,25 @@
 package arrays;
 
-import java.util.Arrays;
-
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(args));
+		Fatura fatura = new Fatura();
+		fatura.numero = 123;
+		fatura.valorTotal = 1_293.55;
 
-		if (args.length <= 1) {
-			System.out.printf("Use: java %s [pt|en] [seu nome]", Principal.class.getName());
-		} else if (args[0].equals("en")) {
-			System.out.printf("Hello, %s!%n", args[1]);
-		} else if (args[0].equals("pt")) {
-			System.out.printf("Olá, %s!%n", args[1]);
-		}
+		ServicoDeCobranca servicoDeCobranca = new ServicoDeCobranca();
+
+//        String[] emailsCobranca = {"joao@algaworks.com", "maria@algaworks.com"};
+//        servicoDeCobranca.pagar(fatura, emailsCobranca);
+
+//        servicoDeCobranca.pagar(fatura,
+//                new String[]{"joao@algaworks.com", "maria@algaworks.com"});
+
+//        servicoDeCobranca.pagar(fatura, new String[0]);
+//        servicoDeCobranca.pagar(fatura, new String[]{});
+
+		servicoDeCobranca.pagar(fatura, "joao@algaworks.com", "maria@algaworks.com");
+//        servicoDeCobranca.pagar(fatura);
 	}
 
 }
